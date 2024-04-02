157b6160b7f379ce39325c103c960f8d6d078b3a
Paul Burgess <paul@burgess.io>
2024-04-02 14:08:23 -0400

    Add functionality to create a new species

--- controllers/species.js
+++ controllers/species.js
@@ -4,9 +4,18 @@ const newSpecies = async (req, res) => {
     res.render('species/new')
 }
 
-
+const create = async(req, res) => {
+    try {
+        await Species.create(req.body)
+        res.redirect('locations')
+   } catch(err) {
+        console.log(err)
+        res.render('species/new', {errorMsg: err.message})
+   }
+}
 
 module.exports = {
     new: newSpecies,
+    create,
 
 }
--- routes/species.js
+++ routes/species.js
@@ -7,7 +7,8 @@ const speciesCtrl = require('../controllers/species');
 //All routes automatically start with /species
 //GET /species/new
 router.get('/new', speciesCtrl.new)
-
+//POST /species
+router.post('/', speciesCtrl.create);
 
 
 
