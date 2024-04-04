41ca133bccd12ab2ffee9c52334cae9af2bed3a6
Paul Burgess <paul@burgess.io>
2024-04-04 17:18:30 -0400

    Remove Lat and Long from locaiton model and new location form

--- models/location.js
+++ models/location.js
@@ -4,8 +4,6 @@ const Schema = mongoose.Schema;
 const locationSchema = new Schema({
     name: String,
     address: String,
-    latitude: Number,
-    longitude: Number,
     website: String,
     wildlife: [{
         type: Schema.Types.ObjectId,
