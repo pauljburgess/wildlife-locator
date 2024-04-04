087e20d1b77d9af86ab2bb700f7c9d2b766d8e95
Paul Burgess <paul@burgess.io>
2024-04-04 16:47:55 -0400

    Recreate Map API functionality

---
+++ Util/location.js
@@ -0,0 +1,22 @@
+// Followed this tutorial: https://www.youtube.com/watch?v=vmOAC0cyFso
+const axios = require('axios')
+const API_KEY = process.env.GOOGLE_MAPS_API_KEY
+
+async function getLatLon(address) {
+    const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${API_KEY}`)
+
+    const data = response.data;
+    
+    if (!data || data.status === 'ZERO_RESULTS') {
+        const error = "No address found"
+        throw error
+    }
+
+    const coordinates = data.results[0].geometry.location
+
+    return coordinates
+}
+
+module.exports = {
+    getLatLon
+}
